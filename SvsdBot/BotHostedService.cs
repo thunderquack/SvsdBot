ccc5e41bc714e59abf844135698e451bec01cf7d
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-06-15 15:27:20 +0200

    comments and constructor

--- SvsdBot/BotHostedService.cs
+++ SvsdBot/BotHostedService.cs
@@ -1,70 +1,86 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using SvsdBot;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
-public class BotHostedService : IHostedService
+namespace SvsdBot
 {
-    private readonly ILogger<BotHostedService> logger;
-    private readonly BotConfiguration botConfig;
-    private ITelegramBotClient botClient;
-
-    public BotHostedService(IOptions<BotConfiguration> botConfig, ILogger<BotHostedService> logger)
+    /// <summary>
+    /// Hosted service for the bot.
+    /// </summary>
+    public class BotHostedService(IOptions<BotConfiguration> botConfig, ILogger<BotHostedService> logger) : IHostedService
     {
-        this.botConfig = botConfig.Value;
-        this.logger = logger;
-    }
+        private readonly ILogger<BotHostedService> logger = logger;
+        private readonly BotConfiguration botConfig = botConfig.Value;
+        private ITelegramBotClient botClient;
 
-    public Task StartAsync(CancellationToken cancellationToken)
-    {
-        this.botClient = new TelegramBotClient(this.botConfig.BotToken);
-
-        var receiverOptions = new ReceiverOptions
+        /// <summary>
+        /// Starts service.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Task.</returns>
+        public Task StartAsync(CancellationToken cancellationToken)
         {
-            AllowedUpdates = Array.Empty<UpdateType>(),
-        };
+            this.botClient = new TelegramBotClient(this.botConfig.BotToken);
 
-        this.botClient.StartReceiving(
-            this.HandleUpdateAsync,
-            this.HandleErrorAsync,
-            receiverOptions,
-            cancellationToken
-        );
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>(),
+            };
 
-        this.logger.LogInformation("Telegram Bot started receiving messages.");
+            this.botClient.StartReceiving(
+                this.HandleUpdateAsync,
+                this.HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
 
-        return Task.CompletedTask;
-    }
+            this.logger.LogInformation("Telegram Bot started receiving messages.");
 
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        this.logger.LogInformation("Telegram Bot stopping.");
-        return Task.CompletedTask;
-    }
-
-    private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-    {
-        if (update.Type == UpdateType.Message && update.Message!.Type == MessageType.Text)
-        {
-            var chatId = update.Message.Chat.Id;
-            var messageText = update.Message.Text;
-
-            this.logger.LogInformation($"Received a '{messageText}' message in chat {chatId}.");
-
-            await botClient.SendTextMessageAsync(
-                chatId: chatId,
-                text: "You said:\n" + messageText,
-                cancellationToken: cancellationToken);
+            return Task.CompletedTask;
         }
-    }
 
-    private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-    {
-        this.logger.LogError(exception, "Error received from Telegram Bot");
-        return Task.CompletedTask;
+        /// <summary>
+        /// Stops the service.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Task.</returns>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("Telegram Bot stopping.");
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message && update.Message!.Type == MessageType.Text)
+            {
+                var chatId = update.Message.Chat.Id;
+                var messageText = update.Message.Text;
+
+                this.logger.LogInformation($"Received a '{messageText}' message in chat {chatId}.");
+
+                await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "You said:\n" + messageText,
+                    cancellationToken: cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// Handles an error if any.
+        /// </summary>
+        /// <param name="botClient">Bot client.</param>
+        /// <param name="exception">Exception.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Task.</returns>
+        private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            this.logger.LogError(exception, "Error received from Telegram Bot");
+            return Task.CompletedTask;
+        }
     }
 }
