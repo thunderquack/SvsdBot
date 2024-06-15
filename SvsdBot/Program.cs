5e96fd87c67df6eae91c7392464862f217e311a7
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-06-15 15:50:18 +0200

    different format for main

--- SvsdBot/Program.cs
+++ SvsdBot/Program.cs
@@ -2,27 +2,49 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using SvsdBot;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((context, config) =>
+namespace SvsdBot
+{
+    /// <summary>
+    /// Main Program.
+    /// </summary>
+    public class Program
     {
-        var env = context.HostingEnvironment;
-        config.SetBasePath(Directory.GetCurrentDirectory());
-        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-              .AddJsonFile($"appsettings.Development.json", optional: true, reloadOnChange: true);
-        config.AddEnvironmentVariables();
-    })
-    .ConfigureServices((context, services) =>
-    {
-        services.Configure<BotConfiguration>(context.Configuration.GetSection("BotConfiguration"));
-        services.AddHostedService<BotHostedService>();
-    })
-    .ConfigureLogging(logging =>
-    {
-        logging.ClearProviders();
-        logging.AddConsole();
-    })
-    .Build();
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+        }
 
-await host.RunAsync();
+        /// <summary>
+        /// Creates and configures a host builder.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>An initialized <see cref="IHostBuilder"/>.</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var env = context.HostingEnvironment;
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                          .AddJsonFile($"appsettings.Development.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.Configure<BotConfiguration>(context.Configuration.GetSection("BotConfiguration"));
+                    services.AddHostedService<BotHostedService>();
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                });
+    }
+}
